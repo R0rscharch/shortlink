33db517f4f806db27afa52004ae700c8888499b2
hxj <1170664169@qq.com>
2024-04-24 17:02:56 +0800

    全局异常处理

---
+++ admin/src/main/java/com/shortlink/admin/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.shortlink.admin.controller;
+
+import com.shortlink.admin.common.convention.result.Result;
+import com.shortlink.admin.common.convention.result.Results;
+import com.shortlink.admin.dto.resp.UserRespDTO;
+import com.shortlink.admin.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 用户管理控制层
+ */
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    /**
+     * 根据用户名查询用户信息
+     */
+    @GetMapping("/api/short-link/admin/v1/user/{username}")
+    public Result<UserRespDTO> getUserByUsername(@PathVariable("username") String username) {
+        UserRespDTO result = userService.getUserByUsername(username);
+        return Results.success(result);
+    }
+}
